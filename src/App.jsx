142e03c898ab97dbda9d17fc42c3cf1154b11e71
faizan <faizankhan9891@gmail.com>
2024-11-19 12:11:33 +0530

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     Hello world I am very excited to deploy my application on docker first time.
+    </>
+  )
+}
+
+export default App
